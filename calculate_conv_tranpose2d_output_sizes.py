2b0d9c82541658feea1111bf23bef6a81c674a29
mlslayer <mlslayergithub@gmail.com>
2019-10-15 22:08:56 -0400

    fix conv2d_transpose_output_sizes

--- calculate_conv_tranpose2d_output_sizes.py
+++ calculate_conv_tranpose2d_output_sizes.py
@@ -10,9 +10,11 @@ kernel_height = int(input('what is the kernel height?'))
 kernel_width = int(input('what is the kernel width?'))
 stride_height = int(input('what is the stride height?'))
 stride_width = int(input('what is the stride width?'))
+output_padding_height = int(input('what is the ouput padding height?'))
+output_padding_width = int(input('what is the ouput padding width?'))
 
-height_out = (height - 1) * stride_height - 2 * padding_height + dilation_height * (kernel_height - 1) + padding_height + 1
-width_out = (width - 1) * stride_width - 2 * padding_width + dilation_width * (kernel_width - 1) + padding_width + 1
+height_out = (height - 1) * stride_height - 2 * padding_height + dilation_height * (kernel_height - 1) + output_padding_height + 1
+width_out = (width - 1) * stride_width - 2 * padding_width + dilation_width * (kernel_width - 1) + output_padding_width + 1
 
 print('height, width is ', height_out, width_out)
 
